41449fe0b34e9429e100bd30b0047f8233e3b2ef
kravets-julia <kravets_julia@i.ua>
2023-06-24 08:45:09 +0300

    dotenv

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-const DB_HOST = (module.exports = {
+const DB_HOST =
+  "mongodb+srv://Julia:GMBgtOeL6YD9JsJS@cluster0.p4bupmv.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+module.exports = {
   DB_HOST,
-});
+};
