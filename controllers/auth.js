600edabd67cb1fd6e8ed9421650d9c6d1f02d359
kravets-julia <kravets_julia@i.ua>
2023-07-07 14:16:54 +0300

    jimp

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const {User} = require("../models/user");
 
@@ -95,6 +96,11 @@ const updateAvatar = async (req, res) => {
   const {path: tempUpload, originalname} = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+
+  await Jimp.read(tempUpload).then((avatar) => {
+    return avatar.resize(250, 250).write(tempUpload);
+  });
+
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, {avatarURL});
